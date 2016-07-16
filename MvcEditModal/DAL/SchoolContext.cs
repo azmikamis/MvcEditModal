1698b18bc31f7438b53a4259dec4130e625887e6
Mohammed Azmi <Azmi.Kamis@MarinaBaySands.com>
2016-07-16 20:46:02 +0800

    Initial commit

---
+++ MvcEditModal/DAL/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using MvcEditModal.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MvcEditModal.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext()
+            : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MvcEditModal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcEditModal.MvcApplication" Language="C#" %>
